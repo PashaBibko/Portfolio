49201f27619e4566f7c40a403fa157362310c576
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-05-25 17:16:03 +0100

    Created the front canvas

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-const greeting: string = "Hello world!";
-document.body.innerHTML = `<h1>${greeting}</h1>`;
+// Gets the canvas and it's rendering context //s
+const canvas = document.getElementById("FrontCanvas") as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+if (!canvas || !ctx) {
+  throw new Error('Error getting canvas elements');
+}
+
+// Fills the canvas to the window size //
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+gradient.addColorStop(0, "red");
+gradient.addColorStop(1, "white");
+
+ctx.fillStyle = gradient;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
